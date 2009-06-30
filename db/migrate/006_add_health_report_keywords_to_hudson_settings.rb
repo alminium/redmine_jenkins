9fe45a7305d9a157c8e8dbb67fd2ab4da22dbd3d
toshiyuki.ando1971 <devnull@localhost>
2009-06-30 13:49:24 +0000

    refs #54 ヘルスレポートのキーワードの既定値を、Hudsonが英語だった場合に設定する値にした。

--- db/migrate/006_add_health_report_keywords_to_hudson_settings.rb
+++ db/migrate/006_add_health_report_keywords_to_hudson_settings.rb
@@ -5,8 +5,8 @@ require 'hudson_settings'
 
 class AddHealthReportKeywordsToHudsonSettings < ActiveRecord::Migration
   def self.up
-    add_column :hudson_settings, :health_report_build_stability, :string, :default => ""
-    add_column :hudson_settings, :health_report_test_result, :string, :default => ""
+    add_column :hudson_settings, :health_report_build_stability, :string, :default => "Build stability"
+    add_column :hudson_settings, :health_report_test_result, :string, :default => "Test Result"
     HudsonSettings.update_all "health_report_build_stability = 'Build stability'"
     HudsonSettings.update_all "health_report_test_result = 'Test Result'"
   end
