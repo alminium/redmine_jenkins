72ffb70cb18c40d832a3fce648d2ce346705c24d
toshiyuki.ando1971 <devnull@localhost>
2010-02-06 05:23:18 +0000

    fixes #291 use add_column and rename_column in place of change_column

--- db/migrate/018_change_column_hudson_build_number.rb
+++ db/migrate/018_change_column_hudson_build_number.rb
@@ -2,10 +2,22 @@
 
 class ChangeColumnHudsonBuildNumber < ActiveRecord::Migration
   def self.up
-    change_column :hudson_builds, :number, :integer
+    add_column :hudson_builds, :number_new, :integer
+    HudsonBuild.find(:all).each do |build|
+      build.number_new = build.number.to_i
+      build.save!
+    end
+    remove_column :hudson_builds, :number
+    rename_column :hudson_builds, :number_new, :number
   end
 
   def self.down
-    change_column :hudson_builds, :number, :string
+    add_column :hudson_builds, :number_old, :string
+    HudsonBuild.find(:all).each do |build|
+      build.number_old = build.number.to_s
+      build.save!
+    end
+    remove_column :hudson_builds, :number
+    rename_column :hudson_builds, :number_old, :number
   end
 end
