43dfeda30e571b535d725758dc33e5e9125d45eb
toshiyuki.ando1971 <devnull@localhost>
2010-03-21 15:38:21 +0000

    fixes #304 remove default value hudson_build_artifacts, display_path, file_name and relative_path

--- db/migrate/015_create_hudson_build_artifacts.rb
+++ db/migrate/015_create_hudson_build_artifacts.rb
@@ -4,9 +4,9 @@ class CreateHudsonBuildArtifacts < ActiveRecord::Migration
   def self.up
     create_table :hudson_build_artifacts do |t|
       t.column :hudson_build_id, :integer
-      t.column :display_path, :text, :default => ""
-      t.column :file_name, :text, :default => ""
-      t.column :relative_path, :text, :default => ""
+      t.column :display_path, :text
+      t.column :file_name, :text
+      t.column :relative_path, :text
     end
   end
 
