d7888db49b7ca4c0e8ea49f6b54f15cfbe741d93
Toshiyuki Ando <couger@kt.rim.or.jp>
2012-05-03 07:57:32 +0900

    fixes #1010 fixes routing error

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
-  map.connect 'projects/:id/:controller/:action', :controller => 'hudson'
-  map.connect 'projects/:id/:controller/:action', :controller => 'hudson_settings'
+  map.connect 'projects/:id/hudson/:action', :controller => 'hudson'
+  map.connect 'projects/:id/hudson_settings/:action', :controller => 'hudson_settings'
 end
