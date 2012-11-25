233caf6d141a1fe872171753fd07990b16d62714
Toshiyuki Ando <couger@kt.rim.or.jp>
2012-11-25 19:49:55 +0900

    refs #1118 remove unused gem

--- features/support/env.rb
+++ features/support/env.rb
@@ -23,8 +23,6 @@ rescue LoadError
 
 end
 
-require 'mocha_standalone'
-
 # Capybara defaults to XPath selectors rather than Webrat's default of CSS3. In
 # order to ease the transition to Capybara we set the default here. If you'd
 # prefer to use XPath just remove this line and adjust any selectors in your
